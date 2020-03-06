10db3815230de76cfe81c11261e8cbef5af23aa7
yangchen <chen.yang3@ymm56.com>
2020-03-06 16:39:02 +0800

    二三维切换功能

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,63 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+    <el-switch
+      style="position:absolute; right: 10px; top: 10px; z-index: 2"
+      v-model="displayModel"
+      active-text="二维"
+      inactive-text="三维"
+    @change="switchModel">
+    </el-switch>
+    <vl-map :load-tiles-while-animating="true" :load-tiles-while-interacting="true" v-show="displayModel">
+      <vl-view :zoom.sync="zoom" :center.sync="center" :rotation.sync="rotation" :max-zoom="maxZoom" :min-zoom="minZoom" :extent="extent"></vl-view>
+      <vl-layer-tile id="osm">
+        <vl-source-osm></vl-source-osm>
+      </vl-layer-tile>
+    </vl-map>
+    <div class="viewer" v-show="!displayModel">
+      <vc-viewer ></vc-viewer>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'App'
+  name: 'App',
+  data () {
+    return {
+      displayModel: true,
+      zoom: 5,
+      center: [106, 34],
+      rotation: 0,
+      maxZoom: 14,
+      minZoom: 3,
+      extent: [-180, -90, 180, 90]
+    }
+  },
+  methods: {
+    switchModel (model) {
+      console.log(model)
+    }
+  },
+  beforeCreate () {
+  }
 }
 </script>
 
 <style>
-#app {
+  html, body, #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+    width: 100%;
+    height: 100%;
+    margin: 0;
+    padding: 0;
 }
+  .viewer{
+    width: 100%;
+    height: 100%;
+  }
 </style>
--- src/main.js
+++ src/main.js
@@ -3,6 +3,18 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import VueLayers from 'vuelayers'
+import 'vuelayers/lib/style.css'
+import ElementUI from 'element-ui' // element-ui的全部组件
+import 'element-ui/lib/theme-chalk/index.css'// element-ui的css
+import VueCesium from 'vue-cesium'
+
+Vue.use(VueLayers, {
+  // global data projection, see https://vuelayers.github.io/#/quickstart?id=global-data-projection
+  dataProjection: 'EPSG:4326'
+})
+Vue.use(ElementUI) // 使用elementUI
+Vue.use(VueCesium)
 
 Vue.config.productionTip = false
 
